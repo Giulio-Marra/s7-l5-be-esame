a86f0d3121cf0de60ffda5ab5043a5b139b0c097
Giulio-Marra <giuliomarramail@gmail.com>
2024-07-05 10:32:48 +0200

    progetto con le dipendenze

---
+++ src/main/java/giulio_marra/s7_l5_be_esame/S7L5BeEsameApplication.java
@@ -0,0 +1,13 @@
+package giulio_marra.s7_l5_be_esame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S7L5BeEsameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S7L5BeEsameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/giulio_marra/s7_l5_be_esame/S7L5BeEsameApplicationTests.java
@@ -0,0 +1,13 @@
+package giulio_marra.s7_l5_be_esame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S7L5BeEsameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
